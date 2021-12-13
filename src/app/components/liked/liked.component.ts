0398f9688300f31a4570ed1c578d21846e9f446a
Edu <emarotomerino@gmail.com>
2021-12-13 15:41:08 +0100

    En la pestaña Liked muestro las zonas en orden de nivel

--- src/app/components/liked/liked.component.ts
+++ src/app/components/liked/liked.component.ts
@@ -1,25 +1,33 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LikedService } from 'src/app/services/liked.service';
 
 @Component({
   selector: 'app-liked',
   templateUrl: './liked.component.html',
-  styleUrls: ['./liked.component.css']
+  styleUrls: ['./liked.component.css'],
 })
 export class LikedComponent implements OnInit {
-
   likedList: any[] = [];
 
   constructor(private likedService: LikedService, private router: Router) {
     this.likedList = this.likedService.getLikedZones();
     //console.log(this.likedList);
+    this.likedList.sort(function (a, b) {
+      if (a.level > b.level) {
+        return 1;
+      }
+      if (a.level < b.level) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
-  goBack(): void{
-    this.router.navigate([".."]);
+  goBack(): void {
+    this.router.navigate(['..']);
   }
 }
